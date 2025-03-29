57ca8b48c215c87cb6c57dd52d2be3cf501b2b1d
Kiso Katsuyuki <katsuyuki2388@gmail.com>
2025-03-29 12:36:21 +0900

    delete the question form and the link to my github page

---
+++ .dir-locals.el
@@ -0,0 +1,19 @@
+((nil . (;; (org-html-toplevel-hlevel . 1)
+	 (eval . (let ((proot (project-root (project-current)))) ;vc-root-dir function doesn't work maybe because vc elisp files have not been loaded.
+		   (setq org-publish-project-alist
+			 `(("notmuch-conf"
+			    ;; Path to your org files.
+			    :base-directory ,(concat proot "docs/org/")
+			    :base-extension "org"
+
+			    ;; Path to your Jekyll project.
+			    :publishing-directory ,(concat proot "docs/")
+			    :recursive t
+			    :publishing-function org-html-publish-to-html
+			    :headline-levels 4
+			    :html-extension "html"
+			    :section-numbers nil
+			    :with-tags nil
+			    :body-only t ;; Only export section between <body> </body>
+			    )))))
+	 )))
